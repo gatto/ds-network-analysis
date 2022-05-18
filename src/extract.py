2539eae3ae12971c05734bf3fa0d8ccf8c2b0770
Fabio <4898010+gatto@users.noreply.github.com>
2022-05-18 10:53:02 +0000

    secrets-first try
    hi

--- src/extract.py
+++ src/extract.py
@@ -1,4 +1,5 @@
 import datetime
+import os
 from pathlib import Path
 
 import pandas as pd
@@ -29,9 +30,12 @@ class SocialETL:
     query: str = field(default="slavaukraini")
     df: pd.DataFrame = field(init=False)
 
+    # get emails and password from environment variables
+
     @df.default
     def _df_default(self):
-        t = Twarc2(bearer_token=self.secret)
+        print(os.environ.get("API_KEY"))
+        t = Twarc2(bearer_token=os.environ.get("API_KEY"))
 
         # Start and end times must be in UTC
         # start_time = datetime.datetime(2022, 3, 21, 0, 0, 0, 0, datetime.timezone.utc)
