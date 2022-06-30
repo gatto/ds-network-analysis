7083490e4ceae58ceeb940e316f94ec84071eba4
Fabio <4898010+gatto@users.noreply.github.com>
2022-06-30 15:57:33 +0000

    Works until line 197, lol

--- py/hashtags.py
+++ py/hashtags.py
@@ -1,5 +1,7 @@
+import json
 import keyword
 import sys
+from collections import defaultdict
 
 import pandas as pd
 from extract import Count, SocialETL
@@ -39,10 +41,10 @@ def create_score(df: pd.DataFrame, one_hashtag: str, root_tags: dict) -> list:
 
     if threshold_support < 1:
         threshold_support = 1
-    print(f"Looking for {one_hashtag} with support {threshold_support}…")
+    # print(f"Looking for {one_hashtag} with support {threshold_support}…")
     mask = df[one_hashtag]
     df = df[mask]
-    print(f"I found {len(df)} tweets with {one_hashtag}")
+    # print(f"I found {len(df)} tweets with {one_hashtag}")
     if len(df) < threshold_support:
         return False
     """for col in df:
@@ -50,20 +52,19 @@ def create_score(df: pd.DataFrame, one_hashtag: str, root_tags: dict) -> list:
             print(f"We have {col} hashtag")"""
     results = []
     # START OF TESTING:
-    print(df.info())
+    """    print(df.info())
     print(df.describe())
     print(df.head())
     print(f"[purple]Dude, here's the breakdown:")
     for tag in root_tags.values():
         print(f"[blue]This is {tag[0]}:")
-        print(df[tag[0]])
+        print(df[tag[0]])"""
     for category, tag_madre in root_tags.items():
         try:
             my_query = construct_query_for_pandas(tag_madre)
-            print(f"The pandas query is: [red]{my_query}")
+            # print(f"The pandas query is: [red]{my_query}")
             temp_df = df.query(my_query)
-            print(f"Holy jezuz: {len(temp_df)} and {len(df)}")
-            input("Waiting for cats to cross the street…")
+            # print(f"Holy jezuz: {len(temp_df)} and {len(df)}")
             results.append(len(temp_df) / len(df))
         except KeyError:
             results.append(0)
@@ -72,28 +73,12 @@ def create_score(df: pd.DataFrame, one_hashtag: str, root_tags: dict) -> list:
     return results
 
 
-def characterize_hashtags(tweet: pd.Series, root_tags: dict) -> str:
-    if root_tags["proukr"] in tweet["tags"]:
-        return "proukr"
-    elif root_tags["prorus"] in tweet["tags"]:
-        return "prorus"
-    elif root_tags["pax"] in tweet["tags"]:
-        return "pax"
-
-
-if __name__ == "__main__":
-    ## set the initial "parent" hashtags for each category
-    tagmadre = {
-        "proukr": ["slavaukraini"],
-        "prorus": ["istandwithputin"],
-        "pax": ["stopwarinukraine"],
-    }
-
+def do_search(tagmadre):
     ## construct the initial query to Twarc
     query_madre = construct_query_for_twarc(tagmadre)
     m = SocialETL(
         query=f"({query_madre}) lang:en",
-        pages=1,
+        pages=5,
         recent=False,
     )
 
@@ -178,32 +163,46 @@ if __name__ == "__main__":
     ## TODO: remember that we still need to account for hashtag support
     ## (i.e. number of tweets supporting that hashtag)
     threshold_certainty = 0.5
-    tags_categorized = {category: [] for category in tagmadre}
+    tags_categorized = defaultdict(list)
     for hashtag, scores in all_hashtags_df.iterrows():
         # print(hashtag, scores)  # TODO: check if the scores are different enough among 3 categories
         # print(scores.idxmax(), scores.max())
         if scores.max() > threshold_certainty:
             tags_categorized[scores.idxmax()].append((hashtag, scores.max()))
 
-    sorted(tags_categorized, key=lambda x: x[1])
+    # tags_categorized.sort(key=lambda x: x[1], reverse=True)
+    tags_categorized = {
+        k: sorted(v, key=lambda item: item[1], reverse=True)
+        for k, v in tags_categorized.items()
+    }
     print(tags_categorized)
+    return tags_categorized
+
+    ## second step: take k top hashtags per category, query again, categorize again
+
+
+if __name__ == "__main__":
+    # params
+    top_results_to_take = 3
+
+    ## set the initial "parent" hashtags for each category
+    tag_madre = {
+        "proukr": ["slavaukraini"],
+        "prorus": ["istandwithputin"],
+        "pax": ["stopwarinukraine"],
+    }
+
+    # code
+    end_results = do_search(tag_madre)
+    end_results = {k: [x[0] for x in v] for k, v in end_results.items()}
+    print(
+        end_results
+    )  ## TODO: the dictionary comprehension doesn't work in recreating the structure of tag_madre
+    end_results = do_search(end_results)
+    with open("hashtags.json", "w", encoding="utf-8") as f:
+        json.dump(end_results, f, ensure_ascii=False, indent=4)
     exit()
 
-    my_results = []
-    results = {"proukr": [], "prorus": [], "pax": []}
-    results_as_series = {}
-    my_tags = []
-
-    tweets_with_hashtag["class"] = tweets_with_hashtag.apply(
-        characterize_hashtags, root_tags=tagmadre, axis=1
-    )
-
-    print(
-        tweets_with_hashtag
-    )  # fatto alla fine:padre->score->k figli->score->del supp<=th->print(tweets_with_hashtags)
-
-    top_results_to_show = 10
-    top_results_to_take = 3
     for category, value in results.items():
         results_as_series[category] = pd.Series(value, name=category)
         print(
@@ -224,53 +223,3 @@ if __name__ == "__main__":
     table.add_column("Count in second search", justify="right", style="magenta")
     table.add_column("Category assigned", style="green")
     console = Console()
-
-    """
-    match i:
-        case 0 | 1:
-            category = "proukr"
-        case 2 | 3:
-            category = "prorus"
-        case 4 | 5:
-            category = "pax"
-    table.add_row(item, str(results_as_series[category].value_counts()[item]), None, None)
-    """
-
-    m = SocialETL(
-        query=f"({my_query}) lang:en",
-        pages=1,
-        recent=False,
-    )
-
-    print(m.df)
-    print(m.df.info())
-
-    console.print(table)
-
-    """
-    for e in repl:
-        df_pro["entities.hashtags"] = df_pro["entities.hashtags"].str.replace(
-            e, "", regex=True
-        )
-    df_pro["entities.hashtags"] = df_pro["entities.hashtags"].apply(str).map(str.strip)
-    df_pro["entities.hashtags"] = (
-        df_pro["entities.hashtags"].apply(str).map(str.split)
-    )  # ,args=('\"\"'))
-    df_hash = df_pro["entities.hashtags"]
-
-    hashtag = list()
-    size = len(df_hash)
-    for i in range(0, size):
-        for e in df_hash[i]:
-            if len(e) >= 4:
-                hashtag.append(e)
-
-    tag = {}
-    for e in hashtag:
-        tag[e] = hashtag.count(e)
-
-    tag_list = sorted((value, key) for (key, value) in tag.items())
-    tag_list = sorted(tag_list, key=lambda x: x[0], reverse=True)
-    tag_sorted = dict([(k, v) for v, k in tag_list])
-    print(tag_sorted)
-    """
